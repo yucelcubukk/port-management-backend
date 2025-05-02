ae2d186ecaafdc483121f65bc5b29225d89525bd
Yücel Çubuk <yucelcubuk851@gmail.com>
2025-05-02 19:36:50 +0300

    ADIM - 1: Backend için ilk adımlar başlatıldı backend kurulumu başlıyor

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Port Backend API çalışıyor');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Sunucu ${PORT} portunda çalışıyor`);
+});
